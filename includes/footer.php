cf968ff93f5f22521ef286c7278645358006269d
Fizzrss <fizars123@gmail.com>
2025-11-12 16:46:54 +0700

    add

--- includes/footer.php
+++ includes/footer.php
@@ -5,11 +5,11 @@
         <div class="row gy-4">
           <div class="col-lg-4 col-md-6 footer-about">
             <a href="index.html" class="logo d-flex align-items-center">
-              <span class="sitename">AgriCulture</span>
+              <span class="sitename">Laboratorium Software Engineering</span>
             </a>
             <div class="footer-contact pt-3">
-              <p>A108 Adam Street</p>
-              <p>New York, NY 535022</p>
+              <p>Gedung Teknik Sipil dan Teknologi Informasi</p>
+              <p>Politeknik Negeri Malang, Malang</p>
               <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
               <p><strong>Email:</strong> <span>info@example.com</span></p>
             </div>
